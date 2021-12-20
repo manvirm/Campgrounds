685ba8d0c3fa530aee21638b036f6dc978afe20a
unknown <manvirm29@gmail.com>
2021-12-19 18:17:35 -0800

    Added basic styling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+//creates reusable code to reduce duplicating code
+const ejsMate = require('ejs-mate');
 //Fake put, patch, delete method
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
@@ -16,6 +18,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
